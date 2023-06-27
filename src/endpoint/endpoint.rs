3f9537af925cbeb0becfc94894a052c75893d310
YassinEldeeb <yassineldeeb94@gmail.com>
2023-06-27 12:06:23 +0300

    fix: type issue + get request from source

--- src/endpoint/endpoint.rs
+++ src/endpoint/endpoint.rs
@@ -1,10 +1,12 @@
-use std::{
-    convert::Infallible,
-    pin::Pin,
-};
+use std::{convert::Infallible, pin::Pin};
 
 use crate::config::EndpointDefinition;
 use hyper::{service::Service, Body};
+
+use async_graphql::http::GraphiQLSource;
+use async_graphql_axum::GraphQLResponse;
+use axum::response::{self, IntoResponse};
+use hyper::{Request, Response};
 
 pub type EndpointRequest = hyper::Request<Body>;
 pub type EndpointResponse = hyper::Response<Body>;
@@ -17,6 +19,10 @@ pub type EndpointFuture = Pin<
              + 'static),
     >,
 >;
+
+async fn graphiql(req: Request<Body>) -> impl IntoResponse {
+    response::Html(GraphiQLSource::build().endpoint(req.uri().path()).finish())
+}
 
 #[derive(Clone, Debug)]
 pub struct EndpointRuntime {
@@ -38,7 +44,8 @@ impl Service<EndpointRequest> for EndpointRuntime {
         &mut self,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Result<(), Self::Error>> {
-        todo!()
+        // Check if the service is ready to handle requests
+        std::task::Poll::Ready(Ok(()))
     }
 
     fn call(&mut self, req: EndpointRequest) -> Self::Future {
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,17 @@ use async_graphql::http::GraphiQLSource;
 use axum::{Router, Server};
 
 use axum::http::Request;
+use axum::response::Response;
 use axum::response::{self, IntoResponse};
 use axum::routing::get;
+use hyper::service::Service;
 use hyper::Body;
+use hyper::{header, StatusCode};
+use std::sync::RwLock;
+use std::{convert::Infallible, sync::Arc};
+
+use axum::response::Result;
+use endpoint::endpoint::{EndpointError, EndpointRequest, EndpointResponse, EndpointRuntime};
 use tracing::debug;
 use tracing_subscriber;
 
@@ -19,16 +27,58 @@ pub async fn graphiql(req: Request<Body>) -> impl IntoResponse {
     response::Html(GraphiQLSource::build().endpoint(req.uri().path()).finish())
 }
 
-// pub async fn handle_post(
-//     State(endpoint_runtime): State<Arc<EndpointRuntime>>,
-//     req: Request<Body>,
-// ) -> impl IntoResponse {
-//     endpoint_runtime.call(req);
-//     println!("handle_post, endpoint_runtime: {:?}, req: {:?}", endpoint_runtime, req);
+pub async fn run_endpoint_runtime(
+    req: EndpointRequest,
+    runtime: &mut EndpointRuntime,
+) -> Result<EndpointResponse, EndpointError> {
+    // Call the service with the request
+    runtime.call(req).await
+}
 
-//     // TODO: Run the actual flow from EndpointRuntime
-//     Ok::<_, Infallible>(Response::new(Body::empty()))
-// }
+pub async fn handle_post(req: Request<Body>) -> Result<impl IntoResponse> {
+    let extension: Option<Arc<RwLock<EndpointRuntime>>> = req
+        .extensions()
+        .get::<Arc<RwLock<EndpointRuntime>>>()
+        .cloned();
+
+    println!("{:?}", extension);
+
+    // return Ok::<_, Infallible>(Response::new("hello world".to_string()));
+    match extension {
+        Some(runtime) => {
+            let (parts, body) = req.into_parts();
+            // Convert the Axum Request into an EndpointRequest
+            let endpoint_req = EndpointRequest::from_parts(parts, body);
+
+            // Lock and obtain mutable access
+            let mut runtime = runtime.write().unwrap();
+
+            // Call the service with the request
+            match runtime.call(endpoint_req).await {
+                Ok(response) => Ok(response),
+                Err(_) => {
+                    // If there was an error, return a 500 response
+                    let response = Response::builder()
+                        .status(StatusCode::INTERNAL_SERVER_ERROR)
+                        .body(Body::from("Internal server error"))
+                        .unwrap();
+
+                    Ok(response)
+                }
+            }
+        }
+        None => {
+            // If there was no EndpointRuntime in the extensions, return a 500 response
+            let response = Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .header(header::CONTENT_TYPE, "application/json")
+                .body(Body::from(r#"{"error": "Missing runtime"}"#))
+                .unwrap();
+
+            Ok::<_, Infallible>(response.into_response())
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
