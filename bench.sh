87e8aa3542ac3f1b0a5c22765b6b987ee425ac07
YassinEldeeb <yassineldeeb94@gmail.com>
2023-07-30 01:45:07 +0300

    wait for CPU cooldown

--- bench.sh
+++ bench.sh
@@ -11,7 +11,50 @@ function get_cpu_usage {
 # Threshold for CPU usage (e.g., 5% above the initial CPU usage)
 CPU_USAGE_THRESHOLD=5.0
 
-# Function to cleanup on exit
+# Record the initial CPU usage
+INITIAL_CPU_USAGE=$(get_cpu_usage)
+# Calculate the CPU usage limit for starting the next test
+CPU_USAGE_LIMIT=$(echo "$INITIAL_CPU_USAGE + $CPU_USAGE_THRESHOLD" | bc)
+
+# cooldown to ensure both K6 benchmarks have fair CPU usage to utilize
+function cool_down_till_initial_cpu_usage {
+    echo "CURRENT CPU USAGE IS: ${get_cpu_usage}"
+
+    echo "Starting cooldown..."
+    MAX_WAIT_TIME=420  # Maximum wait time of 7 minutes
+    START_TIME=$(date +%s)  # Get the current time
+
+    # This loop will check the CPU usage
+    while true; do
+        CURRENT_TIME=$(date +%s)  # Get the current time
+        ELAPSED_TIME=$(($CURRENT_TIME - $START_TIME))
+
+        # If the maximum wait time has been reached, exit the loop
+        if [ $ELAPSED_TIME -ge $MAX_WAIT_TIME ]; then
+            echo "Maximum wait time reached. Proceeding to the next test..."
+            break
+        fi
+
+
+        CPU_USAGE=$(get_cpu_usage)  # Get the current CPU usage
+
+
+        # If the CPU usage is below the limit, exit the loop
+        if (( $(echo "$CPU_USAGE < $CPU_USAGE_LIMIT" | bc -l) )); then
+            break
+        fi
+
+        # Log the current CPU usage
+        echo "WAITING FOR COOLDOWN, CURRENT CPU USAGE IS $CPU_USAGE%, IT SHOULD BE BETWEEN $INITIAL_CPU_USAGE% AND $CPU_USAGE_LIMIT% OR LESS TO START"
+
+        # If the CPU usage is above the threshold, wait for 5 seconds before checking again
+        sleep 5
+    done
+    echo "Cooldown completed."
+    echo "CPU USAGE AFTER COOLDOWN: ${CPU_USAGE}"
+}
+
+# cleanup on exit
 function cleanup {
     echo "Stopping the servers..."
     
@@ -28,6 +71,20 @@ function cleanup {
     
     exit 0
 }
+
+function check_if_server_is_running {
+    echo "Checking $2 availability..."
+    for i in {1..10}
+    do
+        curl -s http://localhost:$1/graphql > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "Conductor $2 is up and running!"
+            break
+        fi
+        sleep 1
+    done
+}
+
 # Handle interrupt signal (e.g., CTRL+C) to stop the servers gracefully
 trap cleanup EXIT SIGINT SIGTERM
 
@@ -41,17 +98,7 @@ echo "Starting the Source server..."
 # Saving the PID of the server process
 SOURCE_SERVER_PID=$!
 
-# Check source server availability
-echo "Checking source server availability..."
-for i in {1..10}
-do
-    curl -s http://localhost:4000/graphql > /dev/null
-    if [ $? -eq 0 ]; then
-        echo "Source server is up and running!"
-        break
-    fi
-    sleep 1
-done
+check_if_server_is_running 4000 "Source Server"
 
 # Building Conductor binary in release mode
 echo "Building Conductor Gateway project..."
@@ -63,34 +110,13 @@ echo "Starting the Conductor server..."
 # Saving the PID of the server process
 SERVER_PID=$!
 
-# Checking server availability
-echo "Checking Conductor server availability..."
-for i in {1..10}
-do
-    curl -s http://localhost:8000/graphql > /dev/null
-    if [ $? -eq 0 ]; then
-        echo "Conductor server is up and running!"
-        break
-    fi
-    sleep 1
-done
+check_if_server_is_running 8000 "Conductor Server"
 
-# Cooldown after compilng and starting the server
-sleep 10
-
-# Record the initial CPU usage
-INITIAL_CPU_USAGE=$(get_cpu_usage)
-# Calculate the CPU usage limit for starting the next test
-CPU_USAGE_LIMIT=$(echo "$INITIAL_CPU_USAGE + $CPU_USAGE_THRESHOLD" | bc)
-
-echo "CPU USAGE AFTER COMPILING AND BEFORE CONDUCTOR K6: ${INITIAL_CPU_USAGE}"
+cool_down_till_initial_cpu_usage
 
 # Running K6 test
 echo "Running K6 test on the Conductor server..."
 k6 run ./benches/actual/k6.js
-
-# Cooldown a bit before compiling and running the necessary services for the baseline
-wait 5
 
 # Building Baseline server binary in release mode
 echo "Building the Baseline Server project..."
@@ -102,51 +128,9 @@ echo "Starting the Baseline server..."
 # Saving the PID of the baseline server process
 BASELINE_SERVER_PID=$!
 
-# Checking baseline server availability
-echo "Checking baseline server availability..."
-for i in {1..10}
-do
-    curl -s http://localhost:8001 > /dev/null
-    if [ $? -eq 0 ]; then
-        echo "Baseline server is up and running!"
-        break
-    fi
-    sleep 1
-done
+check_if_server_is_running 8001 "Baseline Server"
 
-# Cooldown: wait until CPU usage returns to its initial state
-echo "Starting cooldown..."
-MAX_WAIT_TIME=300  # Maximum wait time of 5 minutes
-START_TIME=$(date +%s)  # Get the current time
-
-# This loop will check the CPU usage
-while true; do
-    CURRENT_TIME=$(date +%s)  # Get the current time
-    ELAPSED_TIME=$(($CURRENT_TIME - $START_TIME))
-
-    # If the maximum wait time has been reached, exit the loop
-    if [ $ELAPSED_TIME -ge $MAX_WAIT_TIME ]; then
-        echo "Maximum wait time reached. Proceeding to the next test..."
-        break
-    fi
-
-
-    CPU_USAGE=$(get_cpu_usage)  # Get the current CPU usage
-
-
-    # If the CPU usage is below the limit, exit the loop
-    if (( $(echo "$CPU_USAGE < $CPU_USAGE_LIMIT" | bc -l) )); then
-        break
-    fi
-
-    # Log the current CPU usage
-    echo "WAITING FOR COOLDOWN, CURRENT CPU USAGE IS $CPU_USAGE%, IT SHOULD BE BETWEEN $INITIAL_CPU_USAGE% AND $CPU_USAGE_LIMIT% OR LESS TO START"
-
-    # If the CPU usage is above the threshold, wait for 5 seconds before checking again
-    sleep 5
-done
-echo "Cooldown completed."
-echo "CPU USAGE AFTER COOLDOWN: ${CPU_USAGE}"
+cool_down_till_initial_cpu_usage
 
 # Running K6 test
 echo "Running K6 test on the baseline server..."
@@ -162,6 +146,5 @@ if [ $status -ne 0 ]; then
   cleanup
   exit $status
 fi
-
 # Stop the servers
 cleanup
