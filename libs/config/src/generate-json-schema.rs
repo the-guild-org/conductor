d3ac18f690e74556203065e6e8e7697087dca596
Yassin Eldeeb <yassineldeeb94@gmail.com>
2023-11-19 03:58:01 -0500

    Cargo workspace yassin feedback (#136)
    Co-authored-by: Dotan Simha <dotansimha@gmail.com>

--- libs/config/src/generate-json-schema.rs
+++ libs/config/src/generate-json-schema.rs
@@ -5,7 +5,7 @@ pub fn main() {
     println!("⚙️ Generating JSON schema for Conductor config file...");
     let schema = schema_for!(ConductorConfig);
     let as_string = serde_json::to_string_pretty(&schema).unwrap();
-    println!("✏️ Writing to: crates/config/conductor.schema.json");
-    std::fs::write("crates/config/conductor.schema.json", as_string).unwrap();
+    println!("✏️ Writing to: libs/config/conductor.schema.json");
+    std::fs::write("libs/config/conductor.schema.json", as_string).unwrap();
     println!("✅ Done");
 }
