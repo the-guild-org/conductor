ad44613a419c394ff94cfccf11a78dcb7761c0c5
Dotan Simha <dotansimha@gmail.com>
2023-12-18 08:39:52 +0200

    ok added docs to the website

--- libs/config/src/plugins.rs
+++ libs/config/src/plugins.rs
@@ -6,96 +6,76 @@ use crate::{
     PluginDefinition,
 };
 
-/// The `cors` plugin enables Cross-Origin Resource Sharing (CORS) configuration for your GraphQL API.
+/// The `cors` plugin enables [Cross-Origin Resource Sharing (CORS)](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS) configuration for your GraphQL API.
 ///
 /// By using this plugin, you can define rules for allowing cross-origin requests to your GraphQL server. This is essential for web applications that need to interact with your API from different domains.
 ///
-/// ### Configuration Options
-///
-/// - `allow_credentials`: Specifies whether to include credentials in the CORS headers. Credentials can include cookies, authorization headers, or TLS client certificates.
-///
-/// - `allowed_methods`: Defines the HTTP methods allowed when accessing the resource. This is used in response to a CORS preflight request.
-///
-/// - `allowed_origin`: Determines which origins are allowed to access the resource. It can be a specific origin or a wildcard for allowing any origin.
-///
-/// - `allowed_headers`: Lists the headers allowed in actual requests. This helps in specifying which headers can be used when making the actual request.
-///
-/// - `allow_private_network`: Indicates whether requests from private networks are allowed when originating from public networks.
-///
-/// - `max_age`: Sets the duration for which the results of a preflight request can be cached.
-///
-/// ### Example Usage
-///
-/// ```yaml
-/// cors:
-///   enabled: true
-///   config:
-///     allow_credentials: true
-///     allowed_methods: ["GET", "POST"]
-///     allowed_origin: "https://example.com"
-///     allowed_headers: ["Content-Type", "Authorization"]
-///     allow_private_network: false
-///     max_age: 3600
-/// ```
-#[derive(Deserialize, Serialize, Debug, Clone, JsonSchema)]
-#[schemars(example = "cors_plugin_example")]
+#[derive(Default, Deserialize, Serialize, Debug, Clone, JsonSchema)]
+#[schemars(example = "cors_plugin_example1")]
+#[schemars(example = "cors_plugin_example2")]
+#[schemars(example = "cors_plugin_example3")]
 pub struct CorsPluginConfig {
-    /// Access-Control-Allow-Credentials (default: false)
+    /// `Access-Control-Allow-Credentials`: Specifies whether to include credentials in the CORS headers. Credentials can include cookies, authorization headers, or TLS client certificates.
     /// Indicates whether the response to the request can be exposed when the credentials flag is true.
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[serde(
+        default = "default_boolean_false",
+        skip_serializing_if = "Option::is_none"
+    )]
     pub allow_credentials: Option<bool>,
 
-    /// Access-Control-Allow-Methods (default: Any)
+    /// `Access-Control-Allow-Methods`: Defines the HTTP methods allowed when accessing the resource. This is used in response to a CORS preflight request.
     /// Specifies the method or methods allowed when accessing the resource in response to a preflight request.
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    /// You can also specify a special value "*" to allow any HTTP method to access the resource.
+    #[serde(default = "default_wildcard", skip_serializing_if = "Option::is_none")]
     pub allowed_methods: Option<String>,
 
-    /// Access-Control-Allow-Origin (default: Any)
-    /// Specifies a URI that may access the resource.
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    /// `Access-Control-Allow-Origin`: Determines which origins are allowed to access the resource. It can be a specific origin or a wildcard for allowing any origin.
+    /// You can also specify a special value "*" to allow any origin to access the resource.
+    /// You can also specify a special value "reflect" to allow the origin of the incoming request to access the resource.
+    #[serde(default = "default_wildcard", skip_serializing_if = "Option::is_none")]
     pub allowed_origin: Option<String>,
 
-    /// Access-Control-Allow-Headers (default: Any)
+    /// `Access-Control-Allow-Headers`: Lists the headers allowed in actual requests. This helps in specifying which headers can be used when making the actual request.
     /// Used in response to a preflight request to indicate which HTTP headers can be used when making the actual request.
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    /// You can also specify a special value "*" to allow any headers to be used when making the actual request, and the `Access-Control-Request-Headers` will be used from the incoming request.
+    #[serde(default = "default_wildcard", skip_serializing_if = "Option::is_none")]
     pub allowed_headers: Option<String>,
 
-    /// Access-Control-Expose-Headers (default: Any)
-    /// Used in response to a preflight request to indicate which HTTP headers can be used when making the actual request.
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    /// `Access-Control-Expose-Headers`: The "Access-Control-Expose-Headers" response header allows a server to indicate which response headers should be made available to scripts running in the browser, in response to a cross-origin request.
+    /// You can also specify a special value "*" to allow any headers to be exposed to scripts running in the browser.
+    #[serde(default = "default_wildcard", skip_serializing_if = "Option::is_none")]
     pub exposed_headers: Option<String>,
 
-    /// Access-Control-Allow-Private-Network (default: false)
-    /// Indicates whether the resource allows requests from private networks initiating from a public network.
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    /// `Access-Control-Allow-Private-Network`: Indicates whether requests from private networks are allowed when originating from public networks.
+    #[serde(
+        default = "default_boolean_false",
+        skip_serializing_if = "Option::is_none"
+    )]
     pub allow_private_network: Option<bool>,
 
-    /// Access-Control-Max-Age in seconds (default: empty)
-    /// Indicates how long the results of a preflight request can be cached.
+    /// `Access-Control-Max-Age`: Indicates how long the results of a preflight request can be cached.
     /// This field represents the duration in seconds.
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[serde(default = "defualt_max_age", skip_serializing_if = "Option::is_none")]
     pub max_age: Option<u64>,
 }
 
-impl Default for CorsPluginConfig {
-    fn default() -> Self {
-        CorsPluginConfig {
-            allow_credentials: Some(false),
-            allowed_methods: Some("*".to_string()),
-            allowed_origin: Some("*".to_string()),
-            allowed_headers: Some("*".to_string()),
-            exposed_headers: Some("*".to_string()),
-            allow_private_network: Some(false),
-            max_age: None,
-        }
-    }
+fn default_wildcard() -> Option<String> {
+    Some("*".to_string())
 }
 
-fn cors_plugin_example() -> JsonSchemaExample<PluginDefinition> {
+fn default_boolean_false() -> Option<bool> {
+    Some(false)
+}
+
+fn defualt_max_age() -> Option<u64> {
+    None
+}
+
+fn cors_plugin_example1() -> JsonSchemaExample<PluginDefinition> {
     JsonSchemaExample {
         metadata: JsonSchemaExampleMetadata::new(
-            "CORS Plugin Example",
-            Some("This example demonstrates how to configure the CORS plugin."),
+            "Strict CORS",
+            Some("This example demonstrates how to configure the CORS plugin with a strict list of methods, headers and origins."),
         ),
         example: PluginDefinition::CorsPlugin {
             enabled: Some(true),
@@ -105,6 +85,48 @@ fn cors_plugin_example() -> JsonSchemaExample<PluginDefinition> {
                 allowed_methods: Some("GET, POST".into()),
                 allowed_origin: Some("https://example.com".into()),
                 allowed_headers: Some("Content-Type, Authorization".into()),
+                allow_private_network: Some(false),
+                max_age: Some(3600),
+            }),
+        },
+    }
+}
+
+fn cors_plugin_example2() -> JsonSchemaExample<PluginDefinition> {
+    JsonSchemaExample {
+        metadata: JsonSchemaExampleMetadata::new(
+            "Permissive CORS",
+            Some("This example demonstrates how to configure the CORS plugin with a permissive setup."),
+        ),
+        example: PluginDefinition::CorsPlugin {
+            enabled: Some(true),
+            config: Some(CorsPluginConfig {
+                allow_credentials: Some(true),
+                exposed_headers: Some("*".into()),
+                allowed_methods: Some("*".into()),
+                allowed_origin: Some("*".into()),
+                allowed_headers: Some("*".into()),
+                allow_private_network: Some(true),
+                max_age: Some(3600),
+            }),
+        },
+    }
+}
+
+fn cors_plugin_example3() -> JsonSchemaExample<PluginDefinition> {
+    JsonSchemaExample {
+        metadata: JsonSchemaExampleMetadata::new(
+            "Reflect Origin",
+            Some("This example demonstrates how to configure the CORS plugin with a reflect Origin setup."),
+        ),
+        example: PluginDefinition::CorsPlugin {
+            enabled: Some(true),
+            config: Some(CorsPluginConfig {
+                allow_credentials: Some(true),
+                exposed_headers: Some("*".into()),
+                allowed_methods: Some("GET, POST".into()),
+                allowed_origin: Some("reflect".into()),
+                allowed_headers: Some("*".into()),
                 allow_private_network: Some(false),
                 max_age: Some(3600),
             }),
