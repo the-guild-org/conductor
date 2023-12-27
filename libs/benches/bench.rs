406578b9abd512ff98f8af27ae53a23185c9b27f
Dotan Simha <dotansimha@gmail.com>
2023-12-27 09:18:45 +0200

    ok
    tiny adjustments to k6 setup
    
    try something

--- libs/benches/bench.rs
+++ libs/benches/bench.rs
@@ -1,4 +1,6 @@
+use actix_web::{post, App, HttpResponse, HttpServer, Responder};
 use conductor::run_services;
+use conductor_engine::gateway::ConductorGateway;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use futures::future::join_all;
 use hyper::Client;
@@ -10,16 +12,50 @@ use std::thread;
 use std::time::Duration;
 use tokio::runtime::Runtime;
 
-static START: Once = Once::new();
-static SERVER: Mutex<Option<thread::JoinHandle<Result<(), Error>>>> = Mutex::new(None);
+static GW_ONCE: Once = Once::new();
+static GW_MUTEX: Mutex<Option<thread::JoinHandle<Result<(), Error>>>> = Mutex::new(None);
 
-fn start_server() {
-  START.call_once(|| {
+fn start_gateway() {
+  GW_ONCE.call_once(|| {
     let handle = thread::spawn(|| {
       let rt = Runtime::new().unwrap();
       rt.block_on(run_services(&String::from("./config.yaml")))
     });
-    let mut server = SERVER.lock().expect("Failed to lock SERVER");
+    let mut gw = GW_MUTEX.lock().expect("Failed to lock GW_MUTEX");
+    *gw = Some(handle);
+  });
+}
+static SERVER_ONCE: Once = Once::new();
+static SERVER_MUTEX: Mutex<Option<thread::JoinHandle<Result<(), Error>>>> = Mutex::new(None);
+
+#[post("/graphql")]
+async fn hello() -> impl Responder {
+  HttpResponse::Ok().body(
+    json!({
+      "data": {
+        "country": {
+          "name": "Egypt"
+        }
+      }
+    })
+    .to_string(),
+  )
+}
+
+async fn start_mocked_server() -> std::io::Result<()> {
+  HttpServer::new(|| App::new().service(hello))
+    .bind(("127.0.0.1", 4444))?
+    .run()
+    .await
+}
+
+fn start_server() {
+  SERVER_ONCE.call_once(|| {
+    let handle = thread::spawn(|| {
+      let rt = Runtime::new().unwrap();
+      rt.block_on(start_mocked_server())
+    });
+    let mut server = SERVER_MUTEX.lock().expect("Failed to lock SERVER_MUTEX");
     *server = Some(handle);
   });
 }
@@ -29,9 +65,16 @@ const CONCURRENCY_LEVEL: usize = 10;
 
 fn criterion_benchmark(c: &mut Criterion) {
   start_server();
+  start_gateway();
   thread::sleep(std::time::Duration::from_secs(5));
 
   let rt = Runtime::new().unwrap();
+
+  c.bench_function("request hot path without HTTP server", |b| {
+    b.iter(|| {
+      // let gateway = ConductorGateway::
+    })
+  });
 
   c.bench_function("Single Request /graphql", |b| {
     b.iter(|| {
