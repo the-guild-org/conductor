0c66ff5ad4ef55c7546825d3cd34d7ed1ab5a359
Dotan Simha <dotansimha@gmail.com>
2024-01-11 10:21:34 +0200

    tiny fix

--- bin/cloudflare_worker/src/lib.rs
+++ bin/cloudflare_worker/src/lib.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 use conductor_common::http::{
-  ConductorHttpRequest, HeaderName, HeaderValue, HttpHeadersMap, Method,
+  ConductorHttpRequest, ConductorHttpResponse, HeaderName, HeaderValue, HttpHeadersMap, Method,
 };
 use conductor_config::parse_config_contents;
 use conductor_engine::gateway::{ConductorGateway, GatewayError};
@@ -10,7 +10,48 @@ use tracing_subscriber::fmt::time::UtcTime;
 use tracing_subscriber::prelude::*;
 use tracing_web::MakeConsoleWriter;
 use worker::*;
-async fn run_flow(mut req: Request, env: Env, _ctx: Context) -> Result<Response> {
+
+#[tracing::instrument(level = "debug", skip(url, req))]
+async fn transform_req(url: &Url, mut req: Request) -> Result<ConductorHttpRequest> {
+  let mut headers_map = HttpHeadersMap::new();
+
+  for (k, v) in req.headers().entries() {
+    if let (Ok(hn), Ok(hv)) = (HeaderName::from_str(&k), HeaderValue::from_str(&v)) {
+      headers_map.insert(hn, hv);
+    }
+  }
+
+  let body = req.bytes().await?;
+  let uri = url.to_string();
+  let query_string = url.query().unwrap_or_default().to_string();
+  let method = Method::from_str(req.method().as_ref()).map_err(|e| e.to_string())?;
+
+  Ok(ConductorHttpRequest {
+    body: body.into(),
+    uri,
+    query_string,
+    method,
+    headers: headers_map,
+  })
+}
+
+fn transform_res(conductor_response: ConductorHttpResponse) -> Result<Response> {
+  let mut response_headers = Headers::new();
+  for (k, v) in conductor_response.headers.into_iter() {
+    if let Some(ks) = k {
+      if let Ok(vs) = v.to_str() {
+        response_headers.append(ks.as_str(), vs)?
+      }
+    }
+  }
+
+  Response::from_bytes(conductor_response.body.into()).map(|r| {
+    r.with_status(conductor_response.status.as_u16())
+      .with_headers(response_headers)
+  })
+}
+
+async fn run_flow(req: Request, env: Env, _ctx: Context) -> Result<Response> {
   let conductor_config_str = env.var("CONDUCTOR_CONFIG").map(|v| v.to_string());
   let get_env_value = |key: &str| env.var(key).map(|s| s.to_string()).ok();
 
@@ -24,57 +65,14 @@ async fn run_flow(mut req: Request, env: Env, _ctx: Context) -> Result<Response>
 
       match ConductorGateway::new(&conductor_config).await {
         Ok(gw) => {
-          let url = match req.url() {
-            Ok(url) => url,
-            Err(e) => return Response::error(e.to_string(), 500),
-          };
+          let url = req.url()?;
 
           match gw.match_route(&url) {
             Ok(route_data) => {
-              let mut headers_map = HttpHeadersMap::new();
-
-              for (k, v) in req.headers().entries() {
-                if let (Ok(hn), Ok(hv)) = (HeaderName::from_str(&k), HeaderValue::from_str(&v)) {
-                  headers_map.insert(hn, hv);
-                }
-              }
-
-              let body = match req.bytes().await {
-                Ok(b) => b.into(),
-                Err(e) => return Response::error(e.to_string(), 500),
-              };
-              let uri = url.to_string();
-              let query_string = url.query().unwrap_or_default().to_string();
-              let method = match Method::from_str(req.method().as_ref()) {
-                Ok(m) => m,
-                Err(e) => return Response::error(e.to_string(), 500),
-              };
-
-              let conductor_req = ConductorHttpRequest {
-                body,
-                uri,
-                query_string,
-                method,
-                headers: headers_map,
-              };
-
+              let conductor_req = transform_req(&url, req).await?;
               let conductor_response = ConductorGateway::execute(conductor_req, route_data).await;
 
-              let mut response_headers = Headers::new();
-              for (k, v) in conductor_response.headers.into_iter() {
-                if let Some(ks) = k.map(|k| k) {
-                  if let Ok(vs) = v.to_str() {
-                    response_headers
-                      .append(ks.as_str(), vs)
-                      .map_err(|e| e.to_string())?;
-                  }
-                }
-              }
-
-              Response::from_bytes(conductor_response.body.into()).map(|r| {
-                r.with_status(conductor_response.status.as_u16())
-                  .with_headers(response_headers)
-              })
+              transform_res(conductor_response)
             }
             Err(GatewayError::MissingEndpoint(_)) => {
               Response::error("failed to locate endpoint".to_string(), 404)
