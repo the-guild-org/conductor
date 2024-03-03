cbed2e51f53471da1b0aadc687b0f97667b57ed6
Dotan Simha <dotansimha@gmail.com>
2024-03-03 09:47:50 +0200

    fixes

--- plugins/telemetry/src/lib.rs
+++ plugins/telemetry/src/lib.rs
@@ -4,6 +4,9 @@ mod plugin;
 #[cfg(target_arch = "wasm32")]
 pub mod wasm_reporter;
 
+#[cfg(not(target_arch = "wasm32"))]
+pub mod reporter;
+
 pub use config::TelemetryPluginConfig as Config;
 pub use config::TelemetryTarget as Target;
 pub use plugin::TelemetryPlugin as Plugin;
