95112d2f1e537ff7e7ab49e5bcac90b0141fc1e5
Dotan Simha <dotansimha@gmail.com>
2024-03-21 17:25:42 +0200

    fix website build issues

--- website/next.config.js
+++ website/next.config.js
@@ -2,6 +2,7 @@ import { withGuildDocs } from '@theguild/components/next.config';
 
 /** @type {import("next").Config} */
 export default withGuildDocs({
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
